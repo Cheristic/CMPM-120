bdaf87eb225056a2a4a7bb9aad31e82687e33088
Ethan Heffan <eiheffan@ucsc.edu>
2023-10-14 21:41:12 -0700

    done :)

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add('sfx_rocket');
     }
 
     update() {
@@ -18,6 +19,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
         // fire button
         if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            if (!this.isFiring) this.sfxRocket.play();
             this.isFiring = true;
         }
         // if fired, move up
@@ -26,8 +28,12 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
         // reset on miss
         if(this.y <= borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
